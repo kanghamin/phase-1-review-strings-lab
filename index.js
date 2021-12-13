3bdddadcd5ac439f0cc5f13f712e8651b8eba5b6
kanghamin <gogokhm1@gmail.com>
2021-12-13 16:04:07 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = "The 'currentUser' variable must contain a string";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `"Welcome, ${currentUser[0]}!`;
